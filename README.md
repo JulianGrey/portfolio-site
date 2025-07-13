d408097aed58060d675a52d486d0fc881855629e
Julian Grey <julianrgrey@gmail.com>
2025-07-13 23:12:31 +0100

    Initial set up

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# portfolio-site
-The code for my portfolio site: juliangrey.dev
+# Portfolio Site
+
+A space within the interwebtubes to showcase my ability to communicate with a computer through a keyboard.
---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import stylistic from '@stylistic/eslint-plugin'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/eol-last': ['error'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/no-trailing-spaces': ['error'],
+      '@stylistic/semi': ['error', 'always'],
+    }
+  },
+])
