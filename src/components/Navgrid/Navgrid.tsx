49ce5bc7b08858381f3f0632268ac1fc94e2743a
Julian Grey <julianrgrey@gmail.com>
2025-07-14 22:44:57 +0100

    Rewrite Navgrid for React
    Rewrote the TypeScript code that was powering the Navgrid to make use of React, and convert it into a more true React component.

--- src/components/Navgrid/Navgrid.tsx
+++ src/components/Navgrid/Navgrid.tsx
@@ -1,46 +1,32 @@
 import './Navgrid.scss';
 
-const myCellWidth = 30;
-const horizPlacement = 25;
-const vertPlacement = 35;
+function composeWord(category: string, alignment: string) {
+  const categoryStr = category.toUpperCase();
+  const stringArray = categoryStr.split('');
 
-function displayWord(id: string, direction: string, placement: number, cellWidth: number) {
-  let elementString = "";
-  let styleString = "";
-  let reference = "";
-  const character = id + '-char';
-
-  if (direction === "h") {
-    reference = "left";
-  }
-  else {
-    reference = "top";
-  }
-
-  elementString = '<p class="' + id + ' grid-link ' + direction + 'Word">';
-  styleString = '.' + character + ' { position: absolute; width: ' + cellWidth + 'px; }';
-
-  for(let i = 0; i < id.length; i++) {
-    elementString += '<span class="' + character + ' char' + (i + 1) + '">' + id.charAt(i) + '</span>';
-    styleString += '.' + direction + 'Word .char' + (i + 1) + ' { ' + reference + ': ' + (i * placement) + 'px; }';
-  }
-  elementString += '</p>';
-
-  return elementString;
+  return (
+    <p className={`grid-link ${categoryStr} ${alignment}Word`}>
+      {
+        stringArray.map((char, index) => (
+          <span key={`${char}${index}`} className={`${categoryStr}-char char${ index + 1 }`}>{ char }</span>
+        ))
+      }
+    </p>
+  );
 }
 
-const simple = displayWord("SIMPLE", "h", horizPlacement, myCellWidth);
-const portfolio = displayWord("PORTFOLIO", "h", horizPlacement, myCellWidth);
-const responsive = displayWord("RESPONSIVE", "h", horizPlacement, myCellWidth);
-const creative = displayWord("CREATIVE", "h", horizPlacement, myCellWidth);
-const about = displayWord("ABOUT", "h", horizPlacement, myCellWidth);
-const interactive = displayWord("INTERACTIVE", "v", vertPlacement, myCellWidth);
-const contact = displayWord("CONTACT", "v", vertPlacement, myCellWidth);
+export default function Navgrid() {
+  const about = composeWord("about", "h");
+  const contact = composeWord("contact", "v");
+  const creative = composeWord("creative", "h");
+  const interactive = composeWord("interactive", "v");
+  const portfolio = composeWord("portfolio", "h");
+  const responsive = composeWord("responsive", "h");
+  const simple = composeWord("simple", "h");
 
-const navgridHtml = simple + portfolio + responsive + creative + about + interactive + contact;
-
-const Navgrid = () => (
-  <div className="my-navgrid" dangerouslySetInnerHTML={{__html: navgridHtml}}></div>
-);
-
-export default Navgrid;
+  return (
+    <div className='navgrid'>
+      {about}{contact}{creative}{interactive}{portfolio}{responsive}{simple}
+    </div>
+  );
+}
