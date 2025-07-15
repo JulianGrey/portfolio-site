493d317df0ce5a83156423a88e966492b8389a1c
Julian Grey <julianrgrey@gmail.com>
2025-07-15 17:19:38 +0100

    Change site theme and styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,17 +3,17 @@ import './Footer.scss';
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="container">
+      <div>
         <div className="column-footer">
-          <h3>Julian Grey</h3>
+          <h3 className='bold'>Julian Grey</h3>
           <ul className="no-indent">
-            <li className="no-bullet"><a href="mailto:contact@juliangrey.dev">contact@juliangrey.dev</a></li>
-            <li className="no-bullet"><a href="http://www.linkedin.com/in/julianrgrey" target="_blank">LinkedIn</a></li>
             <li className="no-bullet"><a className="link-github" href="http://github.com/JulianGrey" target="_blank">GitHub</a></li>
+            <li className="no-bullet"><a href="http://www.linkedin.com/in/julianrgrey" target="_blank">LinkedIn</a></li>
+            <li className="no-bullet"><a href="mailto:contact@juliangrey.dev">contact@juliangrey.dev</a></li>
           </ul>
         </div>
         <div className="column-footer">
-          <h3>SiteMap</h3>
+          <h3 className='bold'>SiteMap</h3>
           <ul className="no-indent">
             <li className="no-bullet"><a className="link-home" href="/">Home</a></li>
           </ul>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Navgrid from '../../components/Navgrid/Navgrid';
 import { content } from '../../components/Navgrid/content';
 import './Home.scss';
@@ -6,15 +6,7 @@ import './Home.scss';
 const contents = content;
 
 export default function Home() {
-  const [content, setContent] = useState(<>
-    <p>Hello and welcome to my website. This website is for the purpose of letting you know more about me, through showcasing various projects that I have worked on.</p>
-    <p>This site is currently under construction for a full React.js rewrite, and I apologise for the inconvenience caused.</p>
-    <p>However, please feel free to take a look at my socials to learn more about me, and projects that I have worked on.</p>
-    <ul>
-      <li><a href="https://github.com/JulianGrey" target="_blank">GitHub</a></li>
-      <li><a href="https://www.linkedin.com/in/julianrgrey/" target="_blank">LinkedIn</a></li>
-    </ul>
-  </>);
+  const [content, setContent] = useState(<></>);
   const [selection, setSelection] = useState('');
 
   function handleSelection(category: string) {
@@ -25,10 +17,14 @@ export default function Home() {
     setSelection(category);
   }
 
+  useEffect(() => {
+    handleSelection('about');
+  }, []);
+
   return (
     <div>
       <section className="container">
-        <div className="navgrid-text">
+        <div className="navgrid-content">
           <div className="request-text">
             {content}
           </div>
