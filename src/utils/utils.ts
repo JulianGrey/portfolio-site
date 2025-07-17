da9f3004d46eda8797f2b9b3b90f33c39651370e
Julian Grey <julianrgrey@gmail.com>
2025-07-17 20:35:52 +0100

    Add time-based greeting

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,17 @@
-export function capitalise(word: string) {
+export function capitalise(word: string): string {
   if (!word) return '';
   return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+export function getTimeGreeting(): string {
+  const time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
+  const hours = parseInt(time.split(':')[0], 10);
+
+  if (hours >= 4 && hours < 12) {
+    return 'Good morning';
+  } else if (hours >= 12 && hours < 17) {
+    return 'Good afternoon';
+  } else {
+    return 'Good evening';
+  }
 }
